6ae3804b00d2306a9da8d5b8e895f7241c6d06c8
Yoshihiro Yamazaki <yoya@awm.jp>
2023-09-08 02:25:00 +0900

    smartcrop demo.

--- smartcrop.js
+++ smartcrop.js
@@ -25,31 +25,61 @@ function makeCanvasCheckboard(canvas) {
     ctx.fillRect(0, 0, width, height);
 }
 
+function makeCanvasAutoCrop(canvas, image, aspects) {
+    const { width, height } = image;
+    Object.assign(canvas, { width, height });
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0, width, height);
+    makeCanvasCheckboard(canvas);
+    console.log(aspects);
+    let aspectW = 100, aspectH = 100;
+    switch (aspects[0]) {
+    case "1:1":
+        [aspectW, aspectH ] = [100, 100];
+        break;
+    case "4:3":
+        [aspectW, aspectH ] = [400, 300];
+        break;
+    case "16:9":
+        [aspectW, aspectH ] = [160, 90];
+        break;
+    case "3:4":
+        [aspectW, aspectH ] = [300, 400];
+        break;
+    case "9:16":
+        [aspectW, aspectH ] = [90, 160];
+        break;
+    }
+    smartcrop.crop(image, { width: aspectW, height: aspectH }).then(function(result) {
+        const { topCrop } = result;
+        const [ x, y, w, h ] = [ topCrop.x, topCrop.y,
+                                 topCrop.width, topCrop.height ];
+        ctx.clearRect(x, y, w, h, x, y, w, h);
+        ctx.drawImage(image, x, y, w, h, x, y, w, h);
+        const tc = JSON.stringify(topCrop);
+        result.innerText = tc;
+    });
+}
+
 createApp({
     setup() {
         const canvasRef = ref();
+        const aspects = ref();
         let canvas;
         const image = new Image();
-        image.onload = (e) => {
-            const img = e.target;
-            const { width, height } = img;
-            Object.assign(canvas, { width, height });
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, width, height);
-            makeCanvasCheckboard(canvas);
-            smartcrop.crop(img, { width: 100, height: 100 }).then(function(result) {
-                const { topCrop } = result;
-                const [ x, y, w, h ] = [ topCrop.x, topCrop.y,
-                                         topCrop.width, topCrop.height ];
-                ctx.clearRect(x, y, w, h, x, y, w, h);
-                ctx.drawImage(img, x, y, w, h, x, y, w, h);
-            });
+        image.onload = () => {
+            makeCanvasAutoCrop(canvas, image, {0:"1:1"});
         }
         onMounted(() => {
             canvas = canvasRef.value;
             image.src = "img/l_kutsu_200826inochi01.jpg";
         });
-        const dropFile = async (e) => {
+        const onAspect = (e) => {
+            console.log(e.target.selectedOptions);
+            const opts = Array.from(e.target.selectedOptions).map((e) => e.value);
+            makeCanvasAutoCrop(canvas, image, aspects.value);
+        }
+        const onDrop = async (e) => {
             const file = e.dataTransfer.files[0];
             const reader = new FileReader();
             reader.readAsDataURL(file);
@@ -57,8 +87,8 @@ createApp({
             image.src = reader.result;
         }
         return {
-            canvasRef,
-            dropFile,
+            canvasRef, aspects,
+            onDrop, onAspect,
         }
     }
 }).mount('#app')
